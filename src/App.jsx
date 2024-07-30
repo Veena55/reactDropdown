e4c2465d27137ee2bbd530bde7e8a4a74725222b
VEENA SURYAPRAKASH RAO <veenasrao5@gmail.com>
2024-07-30 06:42:27 +0530

    drop-down

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+import { PiCaretDownBold } from "react-icons/pi"
+
+function App() {
+  const [showDropdwon, setDropdwon] = useState(false);
+
+  return (
+    <>
+      <div className="w-screen h-screen flex justify-center items-center bg-[#070736]">
+        <div className="w-1/4">
+          <div className="bg-white flex items-center px-5 rounded-sm" onMouseOver={() => setDropdwon(!showDropdwon)}>
+            <button className=" py-3 w-full text-left font-medium">Select</button>
+            <PiCaretDownBold className="text-black text-xl" />
+          </div>
+          {showDropdwon &&
+            <div className="w-full mt-1 bg-white p-2 ps-5 font-semibold rounded-sm">
+              <div className="py-2 cursor-pointer" onClick={() => setDropdwon(false)}>
+                <a href="#">Yes</a>
+              </div>
+              <hr />
+              <div className="py-2 cursor-pointer" onClick={() => setDropdwon(false)}>
+                <a href="#">Probably not</a>
+              </div>
+            </div>
+          }
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
